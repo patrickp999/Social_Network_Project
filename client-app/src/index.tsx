b1fb0d95e59b53727388a91f7261e1278b5f86d7
Patrick <patrick.puga@epic.gov>
2020-07-13 00:16:15 -0600

    Routing and cleanup

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -5,10 +5,13 @@ import './app/layout/styles.css';
 import App from './app/layout/App';
 import 'mobx-react-lite/batchingForReactDom';
 import * as serviceWorker from './serviceWorker';
+import ScrollToTop from './app/layout/ScrollToTop';
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <ScrollToTop>
+      <App />
+    </ScrollToTop>
   </BrowserRouter>,
   document.getElementById('root')
 );
